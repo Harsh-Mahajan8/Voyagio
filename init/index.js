e510ba085d6716650092a64e4fd4610913c84196
Harsh-Mahajan8 <harshmahajansk@gmail.com>
2025-11-29 22:19:32 +0530

    updating mongoAltas crediantials

--- init/index.js
+++ init/index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 const initData = require('./data');
 const Listing = require('../model/listing');
 
 async function main() {
     const mongoURL = "mongodb://127.0.0.1:27017/Voyagio";
-    const ATLAS_URL = "mongodb+srv://harshmahajansk8:VqV7ExMHTWMHDs2n@cluster0.qqnsync.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    await mongoose.connect(ATLAS_URL);
+    await mongoose.connect('mongodb+srv://harsh-mahajan:harsh-mahajan@cluster0.quzniat.mongodb.net/?appName=Cluster0');
 };
 
 main()
@@ -13,8 +13,8 @@ main()
     .catch(() => { console.log('There is an error in db connection') });
 
 const initDb = async () => {
-    await Listing.deleteMany();
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "685f89dc279da19026acec3b" }));
+    // await Listing.deleteMany();
+    // initData.data = initData.data.map((obj) => ({ ...obj, owner: "685f89dc279da19026acec3b" }));
     await Listing.insertMany(initData.data);
     console.log('Data is saved');
 }
