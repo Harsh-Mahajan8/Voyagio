2d785e479c8bad8d1086a832dd5d1f4faac109de
Harsh-Mahajan8 <harshmahajansk@gmail.com>
2026-03-18 00:34:55 +0530

    editing mongodb link

--- init/index.js
+++ init/index.js
@@ -4,18 +4,12 @@ const initData = require('./data');
 const Listing = require('../model/listing');
 
 async function main() {
-    const mongoURL = "mongodb://127.0.0.1:27017/Voyagio";
-    await mongoose.connect('mongodb+srv://harsh-mahajan:harsh-mahajan@cluster0.quzniat.mongodb.net/?appName=Cluster0');
-};
+    await mongoose.connect('mongodb+srv://harsh-mahajan:harsh-mahajan@cluster0.etazi.mongodb.net/?appName=Cluster0');
+    console.log('DB is connected');
 
-main()
-    .then(() => console.log('Db is connected'))
-    .catch(() => { console.log('There is an error in db connection') });
-
-const initDb = async () => {
     // await Listing.deleteMany();
-    // initData.data = initData.data.map((obj) => ({ ...obj, owner: "685f89dc279da19026acec3b" }));
     await Listing.insertMany(initData.data);
     console.log('Data is saved');
 }
-initDb();
+
+main().catch((err) => console.log('There is an error in db connection:', err.message));
